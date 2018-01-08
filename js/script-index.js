ca8da6efb0f2a725854528439a2b4d69a98fd219
vanehuanchicay <vanesapardoh@gmail.com>
2018-01-08 15:25:58 -0300

    Etapa 2:Asignar texto al elemento p desde jQuery

--- js/script-index.js
+++ js/script-index.js
@@ -4,6 +4,11 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+
+	$(function printNews(){
+		$("p").append("NUEVAS RECETAS");
+	})
+	
 });
 
 
